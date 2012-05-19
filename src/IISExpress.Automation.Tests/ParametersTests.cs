f618b4882a3446ee2b0265e9a1e501c867374d9f
elemarjr <elemarjr@gmail.com>
2012-05-19 16:58:02 -0300

    Parameters Type and Tests

---
+++ src/IISExpress.Automation.Tests/ParametersTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+using SharpTestsEx;
+
+namespace IISExpress.Automation.Tests
+{
+    [TestFixture]
+    public class ParametersTests
+    {
+        [Test]
+        public void Parameters_DefaultToString_ShouldBeEmpty()
+        {
+            var target = new Parameters();
+            target.ToString().Should().Be.Empty();
+        }
+
+        [Test]
+        [TestCase("port", 8080)]
+        [TestCase("config", "demo.config")]
+        [TestCase("site", "MySite")]
+        [TestCase("siteid", "myId")]
+        [TestCase("path", @"c:\myPath")]
+        public void SetParameter(string name, object value)
+        {
+            var target = new Parameters();
+            var property = typeof (Parameters).GetProperties()
+                .First(p => string.Equals(p.Name, name, StringComparison.InvariantCultureIgnoreCase));
+
+            property.SetValue(target, value, null);
+
+            var expected = string.Format(" /{0}:{1}", name, value);
+
+            target.ToString().Should().Be(expected);
+        }
+
+        [Test]
+        public void SettingTraceLevelToNone_ReturnsEmpty()
+        {
+            var target = new Parameters();
+            target.Trace.Should().Be(TraceLevel.None);
+            target.ToString().Should().Be.Empty();
+        }
+
+        [Test]
+        [TestCase(TraceLevel.Info)]
+        [TestCase(TraceLevel.Error)]
+        [TestCase(TraceLevel.Warning)]
+        public void SettingTraceLevelTo(TraceLevel level)
+        {
+            var target = new Parameters {Trace = level};
+            var expected = " /trace:" + level.ToString().ToLower();
+            target.ToString().Should().Be(expected);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void SettingSystrayTo(bool value)
+        {
+            var target = new Parameters {Systray = value};
+            var expected = value ? " /systray:true" : "";
+            target.ToString().Should().Be(expected);
+        }
+
+    }
+}
---
+++ src/IISExpress.Automation/Parameters.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Text;
+
+namespace IISExpress.Automation
+{
+    public enum TraceLevel
+    {
+        None,
+        Info,
+        Warning,
+        Error
+    }
+
+    public class Parameters
+    {
+        public int Port { get; set; }
+        public string Config { get; set; }
+        public TraceLevel Trace { get; set; }
+        public string Site { get; set; }
+        public string Path { get; set; }
+        public string SiteId { get; set; }
+        public bool Systray { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            if (Port != 0)
+                sb.Append(" /port:" + Port.ToString(CultureInfo.InvariantCulture));
+
+            if (!string.IsNullOrEmpty(Config))
+                sb.Append(" /config:" + Config);
+
+            if (Trace != TraceLevel.None)
+                sb.Append(" /trace:" + Trace.ToString().ToLower());
+
+            if (!string.IsNullOrEmpty(Site))
+                sb.Append(" /site:" + Site);
+
+            if (!string.IsNullOrEmpty(Path))
+                sb.Append(" /path:" + Path);
+
+            if (!string.IsNullOrEmpty(SiteId))
+                sb.Append(" /siteid:" + SiteId);
+
+            if (Systray)
+                sb.Append(" /systray:true");
+
+            return sb.ToString();
+        }
+    }
+}
